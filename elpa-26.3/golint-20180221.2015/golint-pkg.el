ba921f862f557216893fb437f21683146cbc6522
felix <felix@upet.fun>
2019-09-24 11:20:26 +0800

    dele

--- elpa-26.3/golint-20180221.2015/golint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "golint" "20180221.2015" "lint for the Go source code" 'nil :commit "414d861bb4acf565ff8cb05f9906a2283b7dc75a" :url "https://github.com/golang/lint")
