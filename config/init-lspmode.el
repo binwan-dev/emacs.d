40f348806c65f1c1b7545834a033f456a9570c13
binwan <felix@upet.fun>
2021-03-11 00:53:11 +0800

    change python to pyright and add doom-modeline

--- config/init-lspmode.el
+++ config/init-lspmode.el
@@ -1,5 +1,11 @@
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+(use-package lsp-mode
+  :bind (("C-c j" . lsp-find-definition)))
+;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 
 (provide 'init-lspmode)
--- init.el
+++ init.el
@@ -12,8 +12,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(conda-anaconda-home (concat binwan-home "/anaconda3/"))
+ '(custom-safe-themes
+   '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
  '(package-selected-packages
-   '(lsp-pyright helm-projectile projectile window-numbering which-key use-package rainbow-mode rainbow-delimiters powerline omnisharp mmm-mode mc-extras magit lsp-python-ms hungry-delete highlight-indent-guides helm-gtags exec-path-from-shell doom-themes deferred dashboard conda company)))
+   '(doom-modeline lsp-pyright helm-projectile projectile window-numbering which-key use-package rainbow-mode rainbow-delimiters powerline omnisharp mmm-mode mc-extras magit lsp-python-ms hungry-delete highlight-indent-guides helm-gtags exec-path-from-shell doom-themes deferred dashboard conda company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
