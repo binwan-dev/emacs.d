0999a08055be88bb460889d7e8448a400f50af4b
binwan-dev <email@wanbin.tech>
2021-12-30 16:10:20 +0800

    update

--- config/init-font.el
+++ config/init-font.el
@@ -1,16 +1,17 @@
-(let ((emacs-font-size 13)
-      emacs-font-name)
-  (setq emacs-font-name "Fira Code")
-  (when (display-grayscale-p)
-    (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-    (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))
+;; (let ((emacs-font-size 13)
+;;       emacs-font-name)
+;;   (setq emacs-font-name "Fira Code")
+;;   (when (display-grayscale-p)
+;;     (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
+;;     (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))
 
-    (setq nox-doc-tooltip-font (format "%s-%s" emacs-font-name emacs-font-size))
-    )
-  (with-eval-after-load 'doom-modeline
-    (set-face-attribute 'mode-line nil :font "SourceCodePro-12")
-    (set-face-attribute 'mode-line-inactive nil :font "SourceCodePro-12")))
+;;     (setq nox-doc-tooltip-font (format "%s-%s" emacs-font-name emacs-font-size))
+;;     )
+(set-face-attribute 'default nil :font "Fira Code-13")
+(with-eval-after-load 'doom-modeline
+  (set-face-attribute 'mode-line nil :font "SourceCodePro-12")
+  (set-face-attribute 'mode-line-inactive nil :font "SourceCodePro-12"))
 
-(setq line-spacing 0.2)
+(setq-default line-spacing 0.2)
 
 (provide 'init-font)
