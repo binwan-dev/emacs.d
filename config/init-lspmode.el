b0836ec9fe455501bda48360a932eafd7df2beb8
binwan-dev <email@wanbin.tech>
2022-01-03 15:16:05 +0800

    update lspmodel for csharp

--- config/init-lspmode.el
+++ config/init-lspmode.el
@@ -48,14 +48,12 @@
   :hook (ccls . lsp))
 
 ;; set for csharp
-(use-package tree-sitter :ensure t)
-(use-package tree-sitter-langs :ensure t)
-(use-package tree-sitter-indent :ensure t)
+;; (use-package tree-sitter :ensure t)
+;; (use-package tree-sitter-langs :ensure t)
+;; (use-package tree-sitter-indent :ensure t)
 
 (use-package csharp-mode
   :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode))
   :hook (csharp-mode . lsp))
 
 (provide 'init-lspmode)
