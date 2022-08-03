ce234ed3caf9848f030b470b6f479623d4c8da75
binwan-dev <email@wanbin.tech>
2022-08-03 17:38:18 +0800

    update

--- init.el
+++ init.el
@@ -29,4 +29,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t))))
